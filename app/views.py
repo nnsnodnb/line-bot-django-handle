cc9eaadfe55b476af01cabc78c6821945785a094
nnsnodnb <ahr63_gej@me.com>
2018-06-16 13:52:32 +0900

    Update app's source for urls & views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
 
-# Create your views here.
+
+class CallbackView(View):
+
+    def post(self, *args, **kwargs):
+        return HttpResponse()
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(CallbackView, self).dispatch(*args, **kwargs)
