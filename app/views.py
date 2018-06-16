1278ba6e090f1ce8a82c8dab1396848ce7c53311
nnsnodnb <ahr63_gej@me.com>
2018-06-16 14:53:32 +0900

    Modify response code

--- app/views.py
+++ app/views.py
@@ -4,7 +4,8 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from . import handler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from . import line_bot_api, handler
 
 
 class CallbackView(View):
@@ -25,3 +26,11 @@ class CallbackView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(CallbackView, self).dispatch(*args, **kwargs)
+
+    @staticmethod
+    @handler.add(MessageEvent, message=TextMessage)
+    def message_event(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(event.message.text)
+        )
