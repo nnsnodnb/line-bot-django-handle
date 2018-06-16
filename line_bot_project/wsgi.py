511cf4e635528b6969431cd24a85c6dd4fe44111
nnsnodnb <ahr63_gej@me.com>
2018-06-16 14:02:45 +0900

    Add dotenvv reader

--- line_bot_project/wsgi.py
+++ line_bot_project/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+import dotenv
 import os
+import os.path
 
 from django.core.wsgi import get_wsgi_application
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "line_bot_project.settings")
 
--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+import dotenv
 import os
 import sys
 
 if __name__ == "__main__":
+    dotenv.read_dotenv()
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "line_bot_project.settings")
     try:
         from django.core.management import execute_from_command_line
