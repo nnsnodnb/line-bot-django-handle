3a64b400a6f59f7f50dc8a275e39e44d24b36ddd
nnsnodnb <ahr63_gej@me.com>
2018-06-17 16:23:49 +0900

    Add FollowEvent & UnfollowEvent response

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, FollowEvent, UnfollowEvent, TextMessage, TextSendMessage
 from . import line_bot_api, handler
 
 
@@ -18,14 +18,30 @@ class CallbackView(View):
             handler.handle(body, signature)
         except InvalidSignatureError:
             return HttpResponseBadRequest()
-        except LineBotApiError:
-            return HttpResponseServerError()
 
         return HttpResponse('OK')
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(CallbackView, self).dispatch(*args, **kwargs)
+
+    @staticmethod
+    @handler.add(FollowEvent)
+    # https://developers.line.me/en/docs/messaging-api/reference/#follow-event
+    def follow_event(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage('Follow Event')
+        )
+
+    @staticmethod
+    @handler.add(UnfollowEvent)
+    # https://developers.line.me/en/docs/messaging-api/reference/#unfollow-event
+    def unfollow_event(event):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage('Unfollow Event')
+        )
 
     @staticmethod
     @handler.add(MessageEvent, message=TextMessage)
