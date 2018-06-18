af20c245967943dde98cdcf50bb14df18d80ebe1
nnsnodnb <ahr63_gej@me.com>
2018-06-19 01:27:23 +0900

    Add accountLink

--- app/views.py
+++ app/views.py
@@ -5,13 +5,16 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import (
-    MessageEvent, FollowEvent, UnfollowEvent, TextMessage,
+    AccountLinkEvent, MessageEvent, FollowEvent, UnfollowEvent,
+    TextMessage,
     ButtonsTemplate, TemplateSendMessage, TextSendMessage,
     MessageTemplateAction, PostbackTemplateAction, URITemplateAction,
 )
 from . import line_bot_api, handler
 
 import re
+import requests
+import uuid
 
 buttonRegex = re.compile('(ボタン|ぼたん)')
 
@@ -52,6 +55,15 @@ class CallbackView(View):
         )
 
     @staticmethod
+    @handler.add(AccountLinkEvent)
+    def account_link_event(event):
+        if event.link.result == 'ok':
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage('Account Link success.')
+            )
+
+    @staticmethod
     @handler.add(MessageEvent, message=TextMessage)
     def message_event(event):
         if buttonRegex.search(event.message.text):
@@ -80,6 +92,25 @@ class CallbackView(View):
                         ]
                     )
                 )
+            )
+        elif event.message.text == 'アカウント連携':
+            response = requests.post(f'https://api.line.me/v2/bot/user/{event.source.user_id}/linkToken',
+                                     headers=line_bot_api.headers).json()
+            if 'linkToken' not in response:
+                return
+
+            link_token = response['linkToken']
+            nonce = str(uuid.uuid4())
+            success_link = f'https://access.line.me/dialog/bot/accountLink?linkToken={link_token}&nonce={nonce}'
+
+            # デモ用なので連携できたとします
+            line_bot_api.reply_message(
+                event.reply_token,
+                [
+                    TextSendMessage(text=f'こちらにアクセスしてログインしてください\n'
+                                         f'https://example.com/link?linkToken={link_token}'),
+                    TextSendMessage(f'連携成功時のリンク\n{success_link}')
+                ]
             )
         else:
             line_bot_api.reply_message(
